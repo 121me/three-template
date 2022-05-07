4b715ea7020df25ec00327a27d2d2bf7c49b2cb0
121me <125melih125@gmail.com>
2022-05-07 19:22:11 +0300

    updated packages

--- bundler/webpack.common.js
+++ bundler/webpack.common.js
@@ -39,12 +39,14 @@ module.exports = {
 
             // JS
             {
-                test: /\.js$/,
+                test: /\.m?js$/,
                 exclude: /node_modules/,
-                use:
-                [
-                    'babel-loader'
-                ]
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
             },
 
             // CSS
